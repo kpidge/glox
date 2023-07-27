7765d06e4ad081394c27520fa94a02f0524f6211
Kyle Pidgeon <92022089+kpidge@users.noreply.github.com>
2023-07-27 22:53:54 +0100

    Scan operators and literals

--- lox/scanner.go
+++ lox/scanner.go
@@ -1,5 +1,10 @@
 package lox
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type Scanner struct {
 	Source string
 	tokens []Token
@@ -34,7 +39,51 @@ func (s *Scanner) scanToken() {
 		case '+': s.addToken(PLUS)
 		case ';': s.addToken(SEMICOLON)
 		case '*': s.addToken(STAR)
-		default: Error(s.line, "Unexpected character")
+		case '!':
+			token := BANG
+			if s.match('=') {
+				token = BANG_EQUAL
+			}
+			s.addToken(token)
+		case '=':
+			token := EQUAL
+			if s.match('=') {
+				token = EQUAL_EQUAL
+			}
+			s.addToken(token)
+		case '<':
+			token := LESS
+			if s.match('=') {
+				token = LESS_EQUAL
+			}
+			s.addToken(token)
+		case '>':
+			token := GREATER
+			if s.match('=') {
+				token = GREATER_EQUAL
+			}
+			s.addToken(token)
+		case '/':
+			if s.match('/') {
+				for s.peek() != '\n' && !s.isAtEnd() {
+					s.advance()
+				}
+			} else {
+				s.addToken(SLASH)
+			}
+			// Either this, or use s.match instead of peek above
+			fallthrough
+		case ' ':
+		case '\r':
+		case '\t':
+		case '\n': s.line += 1
+		case '"': s.string()
+		default: // This is checked here to save adding a case for each digit
+			if isDigit(c) {
+				s.number()
+			} else {
+				Error(s.line, "Unexpected character")
+			}
 	}
 }
 
@@ -47,6 +96,56 @@ func (s *Scanner) advance() rune {
 	current := s.current
 	s.current += 1
 	return rune(s.Source[current])
+}
+
+func (s *Scanner) match(expected rune) bool {
+	if s.isAtEnd() { return false }
+	if rune(s.Source[s.current]) != expected { return false }
+
+	s.current += 1
+	return true
+}
+
+func (s *Scanner) peek() rune {
+	if s.isAtEnd() { return '\u0000' }
+	return rune(s.Source[s.current])
+}
+
+func (s *Scanner) peekNext() rune {
+	if s.current + 1 >= len(s.Source) { return '\u0000' }
+	return rune(s.Source[s.current+1])
+}
+
+func (s *Scanner) string() {
+	for s.peek() != '"' && !s.isAtEnd() {
+		if s.peek() == '\n' { s.line += 1 }
+		s.advance()
+	}
+
+	if s.isAtEnd() {
+		Error(s.line, "Unterminated string")
+		return
+	}
+
+	s.advance() // Closing "
+	value := s.Source[s.start+1:s.current-1]
+	s.addTokenWithLiteral(STRING, value)
+}
+
+func (s *Scanner) number() {
+	for isDigit(s.peek()) { s.advance() }
+
+	if s.peek() == '.' && isDigit(s.peekNext()) {
+		s.advance()
+		for isDigit(s.peek()) { s.advance() }
+	}
+	f64, err := strconv.ParseFloat(s.Source[s.start:s.current], 64)
+	if err != nil {
+		Error(s.line, fmt.Sprint(err))
+	} else {
+		s.addTokenWithLiteral(NUMBER, f64)
+	}
+
 }
 
 func (s *Scanner) addToken(t TokenType) {
