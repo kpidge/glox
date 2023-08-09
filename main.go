d64fa0df797f30c3325895f2a9f6992826b9a779
Kyle Pidgeon <92022089+kpidge@users.noreply.github.com>
2023-08-09 23:41:56 +0100

    Remove unnecessary print statements

--- main.go
+++ main.go
@@ -7,13 +7,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println(os.Args)
+
 	if len(os.Args) > 2 {
 		fmt.Fprint(os.Stderr, "Usage: glox [script]\n")
 		os.Exit(64)
 	} else if args := os.Args; len(args) == 2 {
-		fmt.Println("Scanning file")
 		lox.RunFile(args[1])
 	} else {
 		lox.RunPrompt()
